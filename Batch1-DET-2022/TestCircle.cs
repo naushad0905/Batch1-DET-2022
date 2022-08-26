7604ba5529a431fe6c756eed36b76bbdaafc1f1f
etg1 <etg1@BG4WS1370>
2022-08-26 18:11:23 +0530

    Changes is done

---
+++ Batch1-DET-2022/TestCircle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestCircle
+    {
+        public static void Main()
+        {
+            Circle a = new Circle(10);
+            Console.WriteLine($"The Area of a circle is {a.Area()}");
+        }
+    }
+}
---
+++ Batch1-DET-2022/TestEmp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestEmp
+    {
+        public static void Main()
+        {
+            Emp e = new Emp(22804, "Syed Naushad", new DateOnly(2020, 8, 1));
+            Console.WriteLine($"Your Years of Experience {e.GetYearsofExp()}");
+        }
+    }
+}
