1bafb3f8cb163e28e4caea7a0cb2776c09695368
etg1 <etg1@BG4WS1370>
2022-08-26 17:13:15 +0530

    Git Changes

---
+++ Batch1-DET-2022/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class Circle
+    {
+        double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Area()
+        {
+            double Area = Math.PI * radius * radius;
+            return (Area);
+        }
+    }
+}
