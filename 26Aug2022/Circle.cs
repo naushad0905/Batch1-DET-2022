b015c6e12b1ab759ec193cbc616f46424b3df6b7
naushad0905 <105446236+naushad0905@users.noreply.github.com>
2022-08-26 16:35:32 +0530

    Add files via upload
    Todays Practiced code Files

---
+++ 26Aug2022/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class Circle
+    {
+        double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Area()
+        {
+           double Area = Math.PI * radius * radius;
+            return (Area);
+        }
+    }
+}
