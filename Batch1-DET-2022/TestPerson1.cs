6a9540be08b1c7cd1b46d02c8a15e30043d6abb2
etg1 <etg1@BG4WS1370>
2022-08-29 18:55:53 +0530

    Person Details

---
+++ Batch1-DET-2022/TestPerson1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestPerson1
+    {
+        public static void Main()
+        {
+            try
+            {
+                Person1 t = new Person1("Syed", "Naushad", "syednaushad@Audi.com", new DateOnly(1998, 05, 09));
+                int byear = t.BirthYear();
+                if (byear < 1900 || byear > 2022)
+                {
+                    throw new InvalidEmpIdException("ENTER VALID BIRTH YEAR");
+                }
+                string r = t.GetSunSign();
+                string s = t.ChineseZodiac();
+                string w = t.IsAdult();
+                string b = t.BDayStatus();
+                string u = t.DefaultUsername();
+                string n = t.GetInfo();
+                Console.WriteLine($"NAME = {n}");
+                Console.WriteLine($"Sun Sign = {r}");
+                Console.WriteLine($"Chinese Zodiac = {s}");
+                Console.WriteLine($"Age Status = {w}");
+                Console.WriteLine($"Birthday Status = {b}");
+                Console.WriteLine($"Default Username = {u}");
+            }
+            catch (InvalidEmpIdException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception m)
+            {
+                Console.WriteLine(m.Message);
+            }
+        }
+    }
+}
